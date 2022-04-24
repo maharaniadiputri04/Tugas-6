02c06752486a3fa7284535430986fd3408a030f9
maharaniadiputri04 <87768425+maharaniadiputri04@users.noreply.github.com>
2022-04-24 22:31:26 +0700

    Add files via upload

---
+++ storage/framework/views/c93db2830746da491de2e5bd512b3d7d89ed60a9.php
@@ -0,0 +1,8 @@
+<?php $__env->startSection('title', 'cobaaaaaaaa'); ?>
+
+<?php $__env->startSection('content'); ?>
+Urutan ke - <?php echo e($ke); ?>
+
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\Toshiba\tugasmaharani\resources\views/coba.blade.php ENDPATH**/ ?>
