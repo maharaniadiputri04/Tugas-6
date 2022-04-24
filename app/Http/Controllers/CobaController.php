0788ecfb2de7711d647f0ca34f14a5e7e322bbfa
maharaniadiputri04 <87768425+maharaniadiputri04@users.noreply.github.com>
2022-04-24 22:20:27 +0700

    Add files via upload

---
+++ app/Http/Controllers/CobaController.php
@@ -0,0 +1,141 @@
+<?php
+
+
+
+namespace App\Http\Controllers;
+
+
+
+use Illuminate\Http\Request;
+
+
+
+class CobaController extends Controller
+
+{
+
+    public function index()
+
+    {
+
+        $friends = Friends::OrderBy('id','desc') -> paginate(3);
+
+
+
+        return view('friends.index', compact('friends'));
+
+    }
+
+    public function create()
+
+    {
+
+    
+
+        return view('friends.create');
+
+    }
+
+    public function store(Request $request)
+
+    {
+
+        // Validate the request...
+
+        $request -> validate ([
+
+            'nama' => 'required|unique:friends|max:255',
+
+            'no_tlp' => 'required|numeric',
+
+            'alamat' => 'required',
+
+        ]);
+
+ 
+
+        $friends = new friends;
+
+ 
+
+        $friends->name = $request->nama;
+
+        $friends->no_tlp = $request->no_tlp;
+
+        $friends->alamat = $request->alamat;
+
+ 
+
+        $friends->save();
+
+
+
+        return redirect('/');
+
+    }
+
+    public function show($Id)
+
+    {
+
+        $friends = friends::where('id', $id) -> first();
+
+        return view('friends.show', ['friend' => $friend]);
+
+    }
+
+
+
+    public function edit($Id)
+
+    {
+
+        $friends = friends::where('id', $id) ->first();
+
+        return view('friends.edit', ['friend' => $friend]);
+
+    }
+
+
+
+    public function update(Request $request, $id)
+
+    {
+
+        $request->validate([
+
+            'nama' => 'required|unique:friends|max:255',
+
+            'no_tlp' => 'required|numeric',
+
+            'alamat' => 'required',
+
+        ]);
+
+        friends::find($id)->update([
+
+        'nama' => $request->nama,
+
+        'no_tlp' => $request->no_tlp,
+
+        'alamat' => $request->alamat
+
+        ]);
+
+
+
+        return redirect('/');
+
+    }
+
+    public function destroy($id)
+
+    {
+
+        friends::find($id)->delete();
+
+        return redirect("/");
+
+    }
+
+}
